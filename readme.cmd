3c7dbf4602a3d6406aba1206070f53ada189ee4b
Karel <145646267+Karel122@users.noreply.github.com>
2025-08-04 14:40:21 +0200

    Update readme.cmd

--- readme.cmd
+++ readme.cmd
@@ -1,6 +1,1 @@
-# Navodila za DOBOT_CR5 z uporabo ROS2
-
-## 1. zaženi v terminalu:
-ros2 launch cr_robot_ros2 dobot_bringup_ros2.launch.py
-Ta serivice vzpostavi povezavo z dobotom.
-## 2.
+# NAVODILA
